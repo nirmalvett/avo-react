398475d778081624a9e5eae04dd552e87daf5108
Lionel Foxcroft <lionelf329@gmail.com>
2018-09-15 16:04:59 -0400

    worked on test creator interface

--- static/js/CreateTest.js
+++ static/js/CreateTest.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import Grid from "@material-ui/core/Grid/Grid";
-import Paper from "@material-ui/core/Paper/Paper";
-import List from "@material-ui/core/List/List";
-import ListSubheader from "@material-ui/core/ListSubheader/ListSubheader";
-import ListItem from "@material-ui/core/ListItem/ListItem";
-import Folder from "@material-ui/icons/Folder";
-import FolderOpen from "@material-ui/icons/FolderOpen";
-import ListItemText from "@material-ui/core/ListItemText/ListItemText";
-import Collapse from "@material-ui/core/Collapse/Collapse";
-import Http from "./Http";
-import Card from "@material-ui/core/Card/Card";
-import LockIcon from "@material-ui/icons/Lock";
-import LockOpenIcon from "@material-ui/icons/LockOpen";
-import DeleteIcon from "@material-ui/icons/Delete";
-import RefreshIcon from '@material-ui/icons/Refresh';
-import CardHeader from "@material-ui/core/CardHeader/CardHeader";
-import IconButton from "@material-ui/core/IconButton/IconButton";
-import ArrowBack from '@material-ui/icons/ArrowBack';
-import ArrowForward from '@material-ui/icons/ArrowForward';
-import Button from "@material-ui/core/Button/Button";
+import Http from './Http';
 import MathJax from 'react-mathjax2'
-import Typography from "@material-ui/core/Typography/Typography";
+import Card from '@material-ui/core/Card/Card';
+import Grid from '@material-ui/core/Grid/Grid';
+import List from '@material-ui/core/List/List';
+import Paper from '@material-ui/core/Paper/Paper';
+import Button from '@material-ui/core/Button/Button';
+import Checkbox from '@material-ui/core/Checkbox/Checkbox';
+import Collapse from '@material-ui/core/Collapse/Collapse';
+import ListItem from '@material-ui/core/ListItem/ListItem';
+import TextField from '@material-ui/core/TextField/TextField';
+import CardHeader from '@material-ui/core/CardHeader/CardHeader';
+import IconButton from '@material-ui/core/IconButton/IconButton';
+import Typography from '@material-ui/core/Typography/Typography';
+import ListItemText from '@material-ui/core/ListItemText/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader/ListSubheader';
+import FormControlLabel from '@material-ui/core/FormControlLabel/FormControlLabel';
+import Done from '@material-ui/icons/Done';
+import Lock from '@material-ui/icons/Lock';
+import Delete from '@material-ui/icons/Delete';
+import Folder from '@material-ui/icons/Folder';
+import Refresh from '@material-ui/icons/Refresh';
+import LockOpen from '@material-ui/icons/LockOpen';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import FolderOpen from '@material-ui/icons/FolderOpen';
+import ArrowForward from '@material-ui/icons/ArrowForward';
 
 
 export default class CreateTest extends React.Component {
@@ -30,7 +34,7 @@ export default class CreateTest extends React.Component {
         this.state = {
             sets: [],
             testQuestions: [],
-            questionIndex: -1,
+            questionIndex: 0
         };
     }
 
@@ -72,34 +76,36 @@ export default class CreateTest extends React.Component {
                 questionIndex: index === 0 && newTestQuestions.length !== 0 ? 0 : index - 1
             });
         };
+        let disableL = index === 0;
+        let disableR = index === this.state.testQuestions.length;
         return (
             <Grid container spacing={8} style={{flex: 1, display: 'flex', marginBottom: 0}}>
                 <Grid item xs={3} style={{flex: 1, display: 'flex'}}>
                     <Paper style={{width: '100%', flex: 1, display: 'flex'}}>
-                        <List style={{flex: 1, overflowY: 'auto', marginTop: '5px', marginBottom: '5px'}} component="nav"
-                              subheader={<ListSubheader component="div">Question Sets</ListSubheader>}>
+                        <List style={{flex: 1, overflowY: 'auto', marginTop: '5px', marginBottom: '5px'}} component='nav'
+                              subheader={<ListSubheader component='div'>Question Sets</ListSubheader>}>
                             {this.state.sets.map((x, y) => [
                                 <ListItem button onClick={() => {
                                     let newSetList = JSON.parse(JSON.stringify(this.state.sets));
-                                    if (newSetList[y].questions.length > 0)
-                                        newSetList[y].open = !newSetList[y].open;
+                                    newSetList[y].open = !newSetList[y].open;
                                     this.setState({sets: newSetList});
-                                }}>
+                                }} disabled={this.state.sets[y].questions.length === 0}>
                                     {x.open ?
                                         <FolderOpen color={x.questions.length === 0 ? 'disabled' : 'action'}/> :
                                         <Folder color={x.questions.length === 0 ? 'disabled' : 'action'}/>}
                                     <ListItemText inset primary={x.name}/>
                                 </ListItem>,
-                                <Collapse in={x.open} timeout="auto" unmountOnExit><List>{
+                                <Collapse in={x.open} timeout='auto' unmountOnExit><List>{
                                     x.questions.map((a) =>
                                         <ListItem button onClick={() => {
                                             let seed = Math.floor(Math.random() * 65536);
                                             Http.getQuestion(a.id, seed, (result) => {
                                                 let newTestQuestions = JSON.parse(JSON.stringify(this.state.testQuestions));
-                                                newTestQuestions.splice(index + 1, 0,
+                                                let newIndex = disableR ? index : index + 1;
+                                                newTestQuestions.splice(newIndex, 0,
                                                     {id: a.id, name: a.name, seed: seed, locked: false,
                                                         prompt: result.prompt, prompts: result.prompts});
-                                                this.setState({testQuestions: newTestQuestions, questionIndex: index + 1});
+                                                this.setState({testQuestions: newTestQuestions, questionIndex: newIndex});
                                             }, () => {});
                                         }}>
                                             <ListItemText secondary={a.name}/>
@@ -110,30 +116,48 @@ export default class CreateTest extends React.Component {
                     </Paper>
                 </Grid>
                 <Grid item xs={1} style={{textAlign: 'center', marginTop: '10%'}}>
-                    <Button variant='fab' onClick={() => {this.setState({questionIndex: index - 1});}}
-                            disabled={index < 1}>
+                    <Button variant='fab' onClick={() => {this.setState({questionIndex: index - 1})}} disabled={disableL}>
                         <ArrowBack style={{width: '100%'}}/></Button></Grid>
                 <Grid item xs={7} style={{display: 'flex'}}>
-                    <Card style={{marginTop: '10%', marginBottom: '10%', padding: '10px', flex: 1}}>{
-                        this.state.testQuestions.length !== 0
-                        ? [<CardHeader
-                                title={currentQ.name}
-                                subheader={'Question ' + (index + 1) + '/' + this.state.testQuestions.length + ', seed=' + currentQ.seed}
-                                action={[
-                                    <IconButton onClick={refresh}><RefreshIcon/></IconButton>,
-                                    currentQ.locked
-                                        ? <IconButton onClick={unlock}><LockIcon/></IconButton>
-                                        : <IconButton onClick={lock}><LockOpenIcon/></IconButton>,
-                                    <IconButton onClick={deleteQ}><DeleteIcon/></IconButton>]}/>,
-                                getMathJax(currentQ.prompt, 'subheading'),
-                                currentQ.prompts.map((x, y) => getMathJax((y+1) + ') ' + x, 'body2'))
+                    <Card style={{marginTop: '5%', marginBottom: '5%', padding: '10px', flex: 1}}>{
+                        index === this.state.testQuestions.length
+                            ? [<CardHeader title={'Test Settings'}
+                                           action={<IconButton><Done/></IconButton>}/>,
+                                <TextField margin='normal' label='Name'
+                                           style={{width: '46%', margin: '2%'}}/>,
+                                <TextField margin='normal' label='Time Limit (minutes)' type='number'
+                                           style={{width: '46%', margin: '2%'}}/>,
+                                <br/>,
+                                <TextField margin='normal' label='Attempts (enter -1 for unlimited)' type='number'
+                                           style={{width: '46%', margin: '2%'}}/>,
+                                <TextField margin='normal' helperText='Deadline' type='datetime-local'
+                                           style={{width: '46%', margin: '2%'}}/>,
+                                <br/>,
+                                <FormControlLabel style={{width: '46%', margin: '2%'}}
+                                    control={<Checkbox checked={this.state.isAssignment}
+                                            onChange={() => this.setState({isAssignment: !this.state.isAssignment})}
+                                            color='primary'
+                                        />
+                                    }
+                                    label='This is an assignment'
+                                />,
                             ]
-                        : <CardHeader title='No Questions Yet' subheader='Click on a question in the sidebar to add one'/>
+                            : [<CardHeader
+                                title={currentQ.name}
+                                subheader={'Question ' + (index + 1) + '/' + this.state.testQuestions.length}
+                                action={[
+                                    <IconButton onClick={refresh}><Refresh/></IconButton>,
+                                    currentQ.locked
+                                        ? <IconButton onClick={unlock}><Lock/></IconButton>
+                                        : <IconButton onClick={lock}><LockOpen/></IconButton>,
+                                    <IconButton onClick={deleteQ}><Delete/></IconButton>]}/>,
+                                getMathJax(currentQ.prompt, 'subheading'),
+                                currentQ.prompts.map((x, y) => getMathJax((y + 1) + ') ' + x, 'body2'))
+                            ]
                     }</Card>
                 </Grid>
                 <Grid item xs={1} style={{textAlign: 'center', marginTop: '10%'}}>
-                    <Button variant='fab' onClick={() => {this.setState({questionIndex: index + 1});}}
-                            disabled={index === this.state.testQuestions.length - 1}>
+                    <Button variant='fab' onClick={() => {this.setState({questionIndex: index + 1})}} disabled={disableR}>
                     <ArrowForward style={{width: '100%'}}/></Button></Grid>
             </Grid>
         );
