a68f5728703e17045f78f4f6f3a4dd20452d473e
Lionel Foxcroft <lionelf329@gmail.com>
2018-09-16 21:09:09 -0400

    Added some of the rendering of test the testing interface

--- static/js/TakeTest.js
+++ static/js/TakeTest.js
@@ -4,6 +4,8 @@ import Grid from "@material-ui/core/Grid/Grid";
 import Card from "@material-ui/core/Card/Card";
 import MathJax from "react-mathjax2";
 import AvoHttp from "./Http";
+import Divider from "@material-ui/core/Divider/Divider";
+import TextField from "@material-ui/core/TextField/TextField";
 
 export default class TakeTest extends React.Component {
     constructor(props) {
@@ -13,29 +15,45 @@ export default class TakeTest extends React.Component {
             this.setState(result);
             }, (result) => alert(result.error));
         this.state = {
-            testID: this.props.testID
+            testID: this.props.testID,
+            questions: [],
         };
     }
-
 
     render() {
         return (
             <Grid container spacing={8}>
                 <Grid xs={1}/>
                 <Grid xs={10} style={{marginTop: '20px', marginBottom: '20px', overflowY: 'auto'}}>
-                    <Card style={{margin: '10px', padding: '10px'}}>
-                        <Typography variant='display1' color='textPrimary'>Welcome to AVO!</Typography>
-                    </Card>
+                    {this.state.questions.map((x, y) => this.getQuestionCard(x, this.state.answers[y]))}
                 </Grid>
                 <Grid xs={1}/>
             </Grid>
         );
     }
 
+    getQuestionCard(question, answer) {
+        return (
+            <Card style={{marginLeft: '10px', marginRight: '10px', marginTop: '20px', marginBottom: '20px', padding: '20px'}}>
+                {this.getMathJax(question.prompt, 'subheading')}
+                {question.prompts.map((x, y) => {
+                    let result = [<Divider style={{marginTop: '10px', marginBottom: '10px'}}/>];
+                    if (x !== '')
+                        result.push(this.getMathJax(x, 'body2'));
+                    result.push(this.getAnswerField(question.types[y], answer[y]));
+                    return result;
+                })}
+            </Card>
+        );
+    }
+
+    getAnswerField(type, answer) {
+        return <TextField/>
+    }
+
     // noinspection JSMethodCanBeStatic
     getMathJax(text, variant) {
-        let strings = text.split(/\\[()\[\]]]/).map((x, y) => y % 2 === 0 ? x :
-            <MathJax.Node inline>{x}</MathJax.Node>);
+        let strings = text.split(/\\[()]/g).map((x, y) => y % 2 === 0 ? x : <MathJax.Node inline>{x}</MathJax.Node>);
         return <MathJax.Context input='tex'><Typography variant={variant}>{strings}</Typography></MathJax.Context>
     }
 }
