f26eb606bd8d82f2527033e14c249e9c17183164
ensmich687 <ens.michael99@gmail.com>
2018-09-22 02:18:14 -0400

    running into a weird compile error with the Takes table will fix later dont merge

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from server import app
+from server import app, db
 
 if __name__ == '__main__':
-    # db.create_all(app=app)
+    db.create_all(app=app)
     app.run()
--- server/__init__.py
+++ server/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask
+from flask_compress import Compress
 
+from server.models import db
 from server.auth import login_manager, UserRoutes
 from server.files import FileRoutes
 from server.routes import routes
-from server.models import db
 
 app = Flask(__name__, static_folder='../static/dist', template_folder='../static')
 login_manager.init_app(app)
 app.config.from_object('config')
 
+db.init_app(app)
 
 app.register_blueprint(UserRoutes)
 app.register_blueprint(FileRoutes)
 app.register_blueprint(routes)
 
-db.init_app(app)
+Compress(app)
