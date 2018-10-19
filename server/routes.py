331ec556c669e5c8dccaf5d8988d9dc4eb90c359
ensmich687 <ens.michael99@gmail.com>
2018-10-19 16:34:45 -0400

    fixed typo

--- server/routes.py
+++ server/routes.py
@@ -202,7 +202,7 @@ def get_test():
     test = Test.query.get(test_id)
     if test is None:
         return jsonify(error='Test not found')
-    if test.is_open = False:
+    if test.is_open is False:
         return jsonify(error='Test Not Open')
     takes = Takes.query.filter((Takes.TEST == test.TEST) & (current_user.USER == Takes.USER) & (Takes.time_submitted > datetime.now())).first()
     if takes is None:
