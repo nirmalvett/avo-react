c46ef7d1649df26d92d878eb529e4e51327f3742
Frank Haolun Li <lifrank1994@gmail.com>
2018-10-25 13:53:01 -0400

    added Vector input

---
+++ static/js/AVOAnswerInput/VectorInput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {getMathJax, sleep, validateMatrix, validateNumber, validateVector} from "../Utilities";
+import FormControlLabel from "@material-ui/core/FormControlLabel/FormControlLabel";
+import Radio from "@material-ui/core/Radio/Radio";
+import TextField from "@material-ui/core/TextField/TextField";
+import Typography from "@material-ui/core/Typography/Typography";
+
+export default class AnswerInput extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+      
+    }
+
+}
