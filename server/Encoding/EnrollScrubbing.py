cb433306db13d5e7ffb6b51e983484214f58fc40
ensmich687 <ens.michael99@gmail.com>
2018-12-06 15:08:58 -0500

    comments and added finish print

--- server/Encoding/EnrollScrubbing.py
+++ server/Encoding/EnrollScrubbing.py
@@ -2,20 +2,25 @@ from server.models import *
 from datetime import datetime
 
 if __name__ == "main":
-    current_time = datetime.now()
+    current_time = datetime.now()  # Current time to delete enrolled from
     expired_transactions = Transaction.query.filter((Transaction.expiration <= current_time) &
                                                     (Transaction.expired is False)).all()
     for trans in expired_transactions:
-        trans.expired = True
-        current_enrolled = None
+        # For each expired transaction remove enrolled
+        trans.expired = True  # Set the current transaction to expired
+        current_enrolled = None  # current enroll to delete
         try:
+            # See if the enroll exists
             current_enrolled = enrolled.query.filter((trans.USER == enrolled.c.USER) &
                                                      (trans.CLASS == enrolled.c.CLASS)).first()
         except:
             print("No results Found")
 
         if enrolled is None:
+            # if enrolled isnt found move onto the next
             pass
         else:
+            # Commit to database
             db.session.delete(current_enrolled)
             db.session.commit()
+    print("Enroll Update Finished")
