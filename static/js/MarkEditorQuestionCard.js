20600a6257213366a5347118622fdf0787613427
Frank Haolun Li <lifrank1994@gmail.com>
2018-12-14 13:08:22 -0500

    restructured mark editor so that it's not using lists but using React Fragments

--- static/js/MarkEditorQuestionCard.js
+++ static/js/MarkEditorQuestionCard.js
@@ -19,7 +19,12 @@ export default class MarkEditorQuestionCard extends React.Component {
         };
     };
 
+    componentDidMount(){
+        this.uniqueKey1 = uniqueKey();
+    }
+
     render() {
+        const uniqueKey1 = this.uniqueKey1;
         return (
             <Card key = { uniqueKey() } style={{marginLeft: '10px', marginRight: '10px', marginTop: '20px', marginBottom: '20px', padding: '20px', position : 'relative'}}>
                 <CardHeader title={getMathJax(this.props.question.prompt)} style={{position: 'relative'}} action={
@@ -27,32 +32,40 @@ export default class MarkEditorQuestionCard extends React.Component {
                         {this.state.buttonMarkValue.reduce((a, b) => a+b, 0)}/{this.props.question.totals.reduce((a, b) => a+b, 0)}
                     </Typography>
                 }/>
-                {this.props.question.prompts.map((x, y) => [
-                    <Divider key = { uniqueKey() } style={{marginTop: '10px', marginBottom: '10px'}}/>,
-                    <div key = { uniqueKey() } style={{position: 'relative'}}>
-                        <div style={{position: 'absolute', right: '8px', top: '8px'}}>
-                            <IconButton onClick={() => this.handleClick(this.state.buttonMarkValue[y], this.props.index, y)}>
-                                {this.state.buttonMarkValue[y] == 1 ? (
-                                    <Check/>
-                                ) : (
-                                    <Close/>
-                                )}
-                            </IconButton>
+
+                {this.props.question.prompts.map((x, y) =>
+                    <React.Fragment>
+                        <Divider key = { `Divider-Index:${y}Key:${uniqueKey1}` } style={{marginTop: '10px', marginBottom: '10px'}}/>
+                        <AnswerInput
+                            key = {  `AnswerInput-Index:${y}Key:${uniqueKey1}`  }
+                            disabled type={this.props.question.types[y]} value={this.props.question.answers[y]} prompt={x}/>
+                    </React.Fragment>
+                    )}
+                {this.props.question.explanation.map((x, y) =>
+                    <React.Fragment>
+                        <Divider key = {  `Divider-Explanation-Index:${y}Key:${uniqueKey1}`  } style={{marginTop: '10px', marginBottom: '10px'}}/>
+
+                        <div key = {  `OuterMarkEditorDiv-Index:${y}Key:${uniqueKey1}`  } style={{position: 'relative'}}>
+                            <div style={{position: 'absolute', right: '8px', top: '8px'}}>
+                                <IconButton onClick={() => this.handleClick(this.state.buttonMarkValue[y], this.props.index, y)}>
+                                    {this.state.buttonMarkValue[y] === 1 ? (
+                                        <Check/>
+                                    ) : (
+                                        <Close/>
+                                    )}
+                                </IconButton>
+                            </div>
                         </div>
-                    </div>,
-                    <AnswerInput key = { uniqueKey() } disabled type={this.props.question.types[y]} value={this.props.question.answers[y]} prompt={x}/>
-                ])}
-                {this.props.question.explanation.map((x, y) => [
-                    <Divider key = { uniqueKey() } style={{marginTop: '10px', marginBottom: '10px'}}/>,
-                    <div>{getMathJax(x)}</div>,
-                ])}
+                        <div>{getMathJax(x)}</div>
+                    </React.Fragment>
+                )}
             </Card>
         );
     };
 
     handleClick(score, index, idx) {
         let newArray = this.state.buttonMarkValue;
-        let newScore = score == 0 ? 1 : 0;
+        let newScore = score === 0 ? 1 : 0;
         newArray[idx] = newScore;
         this.setState({ buttonMarkValue : newArray });
         this.props.markButtonMarkers[index] = newArray;
