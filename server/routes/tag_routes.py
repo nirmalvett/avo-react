bede28dbf57cf7971e6db5c5af5d26ec0eb71df0
Frank Li <lifrank1994@gmail.com>
2019-05-20 16:39:20 -0400

    Update tag_routes.py
    routes tag

--- server/routes/tag_routes.py
+++ server/routes/tag_routes.py
@@ -12,14 +12,26 @@ QuestionRoutes = Blueprint('TagRoutes', __name__)
 # Get sets/questions
 
 
-@QuestionRoutes.route('/getSets')
+@QuestionRoutes.route('/getTags')
 @teacher_only
-def get_tags():
+def get_tags_route():
     """
     For now this route will return all tags from the database
     :return: The list of tags
     """
-    # Get list of available sets for current user
-    # list_of_sets = Set.query.all()
-    # return jsonify(sets=set_list)
+    return jsonify(tags=get_tags())
 
+
+def get_tags():
+    """
+    For now this route will return all tags from the database
+    :return: List, of dict objects each of which represents a tag
+    """
+    # Get list of available sets for current user
+    list_of_tags = Tag.query.all()  # [Tag, Tag...]
+    list_dict = []
+    for tag in list_of_tags:
+        tag_dict = tag.__dict__
+        tag_dict.pop('_sa_instance_state')
+        list_dict.append(tag_dict)
+    return list_dict
