4e72b3c70ca41e881fb0a9fbbd5d368dda81c5f6
Owen Christie <ochrist2@uwo.ca>
2019-07-20 20:53:39 -0400

    added blank component for test component, changed lesson card name oops

--- static/js/Learn/LearnLessonCard.tsx
+++ static/js/Learn/LearnLessonCard.tsx
@@ -4,7 +4,7 @@ import { getMathJax } from '../HelperFunctions/Utilities'
 import { uniqueKey } from '../HelperFunctions/Helpers'
 import * as Models from '../Models'
 import Card from '@material-ui/core/Card/Card';
-export default function LearnQuestionCard(props: Models.LearnLessonCardProps) {
+export default function LearnLessonCard(props: Models.LearnLessonCardProps) {
     return (
         <Card>
             <Typography color={props.color}>{getMathJax(props.title, props.promptVariant, uniqueKey())}</Typography>;
---
+++ static/js/Learn/LearnTestComponent.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Typography } from '@material-ui/core';
+
+
+export default class LearnTestComponent extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
