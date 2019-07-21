a1d0490ea468ec40fa9cabee5767ea8cede6ee74
Owen Christie <ochrist2@uwo.ca>
2019-07-20 20:50:07 -0400

    added file for explanation card that does nothing

---
+++ static/js/Learn/LearnExplanationCard.tsx
@@ -0,0 +1,14 @@
+import { Typography } from '@material-ui/core';
+import React from 'react'
+import { getMathJax } from '../HelperFunctions/Utilities'
+import { uniqueKey } from '../HelperFunctions/Helpers'
+import * as Models from '../Models'
+import Card from '@material-ui/core/Card/Card';
+export default function LearnExplanationCard(props: Models.LearnLessonCardProps) {
+    return (
+        <Card>
+            <Typography color={props.color}>{getMathJax(props.title, props.promptVariant, uniqueKey())}</Typography>;
+            <Typography color={props.color}>{getMathJax(props.body, props.promptVariant, uniqueKey())}</Typography>;
+        </Card>
+    )
+}
