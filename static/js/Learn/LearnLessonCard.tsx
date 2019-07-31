f4ab076c0f006ea101f1e5cb4e1bbb9d23baf700
Owen Christie <ochrist2@uwo.ca>
2019-07-31 18:57:04 -0400

    made lesson and question card look better, verified they work

--- static/js/Learn/LearnLessonCard.tsx
+++ static/js/Learn/LearnLessonCard.tsx
@@ -1,14 +1,14 @@
-import { Typography } from '@material-ui/core';
-import React from 'react'
-import { getMathJax } from '../HelperFunctions/Utilities'
-import { uniqueKey } from '../HelperFunctions/Helpers'
-import * as Models from '../Models'
-import Card from '@material-ui/core/Card/Card';
+import { Typography } from "@material-ui/core";
+import React from "react";
+import { getMathJax } from "../HelperFunctions/Utilities";
+import { uniqueKey } from "../HelperFunctions/Helpers";
+import * as Models from "../Models";
+import Card from "@material-ui/core/Card/Card";
 export default function LearnLessonCard(props: Models.LearnLessonCardProps) {
-    return (
-        <Card>
-            <Typography color={props.color}>{getMathJax(props.title, props.promptVariant, uniqueKey())}</Typography>;
-            <Typography color={props.color}>{getMathJax(props.body, props.promptVariant, uniqueKey())}</Typography>;
-        </Card>
-    )
+  return (
+    <Card style={{padding: 50}}>
+      <div>{getMathJax(props.title, props.promptVariant, uniqueKey())}</div>
+      <div>{getMathJax(props.body, props.promptVariant, uniqueKey())}</div>
+    </Card>
+  );
 }
