89b901db1ab1e534d911637f5ec41453b2505b87
Lionel Foxcroft <lionelf329@gmail.com>
2019-08-12 23:35:20 -0400

    Fix compilation errors

--- static/js/Learn/AVOLessonSlider.jsx
+++ static/js/Learn/AVOLessonSlider.jsx
@@ -5,12 +5,12 @@ import AVOLearnTestComp     from './AVOLearnTestComp';
 import Icon                 from '@material-ui/core/Icon';
 import Grid                 from '@material-ui/core/Grid';
 import Card                 from '@material-ui/core/Card';
-import Http                 from '../HelperFunctions/Http';
+import * as Http            from '../Http';
 import Typography           from '@material-ui/core/Typography';
 import IconButton           from '@material-ui/core/IconButton';
 
 export default class AVOLessonSlider extends Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {
@@ -18,14 +18,14 @@ export default class AVOLessonSlider extends Component {
             currentIndex  : 0,
             changedCurrency: 0,
         };
-        
+
         this.processSlidesIntoGroups = this.processSlidesIntoGroups.bind(this);
         this.getSlideTranslation     = this.getSlideTranslation    .bind(this);
         this.getSlideRenderable      = this.getSlideRenderable     .bind(this);
         this.goToPreviousSlide       = this.goToPreviousSlide      .bind(this);
         this.goToNextSlide           = this.goToNextSlide          .bind(this);
         this.openLessonFSM           = this.openLessonFSM          .bind(this);
-        
+
         this.slides = this.processSlidesIntoGroups(this.props.slides);
         this.fsmRef = React.createRef();
     };
@@ -35,10 +35,10 @@ export default class AVOLessonSlider extends Component {
             <Grid container size={12} id='avo-lesson__layout-div'>
                 <Grid xs={1} style={{ zIndex : 10 }}>
                     <center>
-                        <IconButton 
-                            aria-label="chevron_left" 
-                            onClick={this.goToPreviousSlide} 
-                            color="primary" 
+                        <IconButton
+                            aria-label="chevron_left"
+                            onClick={this.goToPreviousSlide}
+                            color="primary"
                             style={{ marginTop : '25vh' }}
                         >
                             <Icon>chevron_left</Icon>
@@ -50,10 +50,10 @@ export default class AVOLessonSlider extends Component {
                 </Grid>
                 <Grid xs={1} style={{ zIndex : 10 }}>
                     <center>
-                        <IconButton 
-                            aria-label="chevron_right" 
-                            onClick={this.goToNextSlide} 
-                            color="primary" 
+                        <IconButton
+                            aria-label="chevron_right"
+                            onClick={this.goToNextSlide}
+                            color="primary"
                             style={{ marginTop : '25vh' }}
                         >
                             <Icon>chevron_right</Icon>
@@ -62,7 +62,7 @@ export default class AVOLessonSlider extends Component {
                 </Grid>
                 <AVOLessonFSM changeToNewMastery={this.props.changeToNewMastery} ref={this.fsmRef}>
                     {!!this.state.currentLesson && (
-                        <AVOLearnTestComp 
+                        <AVOLearnTestComp
                             lesson={this.state.currentLesson} updateMastery={this.props.updateMastery}
                         />
                     )}
@@ -92,21 +92,21 @@ export default class AVOLessonSlider extends Component {
                             }}
                             id= {`avo-lesson__card-${LIndex}-${gIndex}`}
                             key={`avo-learn__card-key:${LIndex}`}
-                        >   
-                            <IconButton 
-                                onClick={(e) => this.openLessonFSM(e, lesson, `${LIndex}-${gIndex}`)} 
-                                color="primary" 
-                                aria-label="fullscreen" 
-                                style={{ 
-                                    position : 'absolute', 
-                                    right    : '0.125em', 
+                        >
+                            <IconButton
+                                onClick={(e) => this.openLessonFSM(e, lesson, `${LIndex}-${gIndex}`)}
+                                color="primary"
+                                aria-label="fullscreen"
+                                style={{
+                                    position : 'absolute',
+                                    right    : '0.125em',
                                     top      : '0.125em',
-                                    zIndex   : '10' 
+                                    zIndex   : '10'
                                 }}
                             >
                                 <Icon>fullscreen</Icon>
                             </IconButton>
-                            <AVOMasteryGauge 
+                            <AVOMasteryGauge
                                 comprehension={parseInt(parseFloat(lesson.newMastery || lesson.mastery) * 100)}
                                 colors={['#399103', '#039124', '#809103']}
                             />
@@ -128,7 +128,7 @@ export default class AVOLessonSlider extends Component {
             );
         });
         return output;
-    }; 
+    };
 
     getSlideTranslation = (index) => {
         if(index < this.state.currentIndex)
@@ -140,14 +140,14 @@ export default class AVOLessonSlider extends Component {
 
     goToPreviousSlide = () => {
         const currentIndex = this.state.currentIndex;
-        if(currentIndex == 0) 
+        if(currentIndex == 0)
             return;
         this.setState({ currentIndex : currentIndex - 1 });
     };
 
     goToNextSlide = () => {
         const currentIndex = this.state.currentIndex;
-        if(currentIndex > this.slides.length - 2) 
+        if(currentIndex > this.slides.length - 2)
             return;
         this.setState({ currentIndex : currentIndex + 1 });
     };
@@ -162,8 +162,8 @@ export default class AVOLessonSlider extends Component {
                 groupCounter++;
             };
             output[groupCounter].push(slide);
-            if(slideCounter == 2) 
-                slideCounter = 0; 
+            if(slideCounter == 2)
+                slideCounter = 0;
             else
                 slideCounter++;
         });
@@ -172,9 +172,9 @@ export default class AVOLessonSlider extends Component {
 
     openLessonFSM = (event, lesson, LIndex) => {
         Http.getLessonData(
-            lesson.ID, 
-            (res) => { 
-                console.log(res); 
+            lesson.ID,
+            (res) => {
+                console.log(res);
                 lesson.data = res;
                 this.setState({ currentLesson : lesson });
                 this.fsmRef.current.handleFSM(event, lesson, LIndex);
