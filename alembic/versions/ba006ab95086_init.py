80e3e20fd02ff803c97316d5733aa3003357842a
Owen <owendot2011@gmail.com>
2019-09-06 21:40:52 -0400

    got migrations working

---
+++ alembic/versions/ba006ab95086_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: ba006ab95086
+Revises: 
+Create Date: 2019-09-06 19:37:42.420893
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ba006ab95086'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
