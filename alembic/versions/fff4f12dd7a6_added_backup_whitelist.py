60deb6d335e6f5243bffbbd8ed9dbf473767de30
Owen <owendot2011@gmail.com>
2019-09-06 21:40:52 -0400

    got migrations working

---
+++ alembic/versions/fff4f12dd7a6_added_backup_whitelist.py
@@ -0,0 +1,24 @@
+"""added backup whitelist
+
+Revision ID: fff4f12dd7a6
+Revises: ba006ab95086
+Create Date: 2019-09-06 19:38:20.812403
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fff4f12dd7a6'
+down_revision = 'ba006ab95086'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
