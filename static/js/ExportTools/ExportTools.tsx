c6796b5c3e192e156a24db7d8bc8e908f6b5a38f
Owen <owendot2011@gmail.com>
2019-09-11 14:00:16 -0400

    fix

--- static/js/ExportTools/ExportTools.tsx
+++ static/js/ExportTools/ExportTools.tsx
@@ -105,6 +105,7 @@ export default class ExportTools extends Component<ExportToolsProps, ExportTools
                     display: 'flex',
                     justifyContent: 'flex-start',
                     flexDirection: 'column',
+                    overflow: 'auto',
                 }}
             >
                 <form
