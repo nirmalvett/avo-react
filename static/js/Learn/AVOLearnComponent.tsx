6cb7e5ab4dbbe3b45e702cbcfb97c161b498fdad
Owen <owendot2011@gmail.com>
2019-10-07 23:50:27 -0400

    selecting screen works now, just need to hook up course selector to the route that fetches lessons

--- static/js/Learn/AVOLearnComponent.tsx
+++ static/js/Learn/AVOLearnComponent.tsx
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
 import * as Http from '../Http';
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Input from '@material-ui/core/Input';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
 import AVOLessonSlider from './AVOLessonSlider';
 import { Slider } from '@material-ui/core';
+
+import {Class} from '../Models';
 
 export interface AvoLesson {
     mastery: number;
@@ -28,6 +34,11 @@ interface AVOLearnComponentState {
     lessons: AvoLesson[];
     allLessons: AvoLesson[];
     filterInput: string;
+    classNames: string[];
+    classes: Class[];
+    selectedClass: Class;
+    selectedClassName: string;
+    otherView: 'Completed' | 'To Do';
 }
 
 export default class AVOLearnComponent extends Component<
@@ -40,6 +51,11 @@ export default class AVOLearnComponent extends Component<
             lessons: [],
             filterInput: '',
             allLessons: [],
+            classNames: [],
+            classes: [],
+            selectedClass: {} as Class,
+            selectedClassName: '',
+            otherView: 'Completed',
         };
     }
 
@@ -55,56 +71,178 @@ export default class AVOLearnComponent extends Component<
                     padding: '1em',
                     position: 'relative',
                     width: '98% !important',
+                    overflowY: 'auto',
+                    overflowX: 'hidden',
                 }}
                 id='avo-learn__layout-div'
             >
-                {(this.state.lessons.length !== 0) && <AVOLessonSlider
-                    theme={this.props.theme}
-                    changeToNewMastery={() => this.changeToNewMastery()}
-                    slides={this.state.lessons}
-                    updateMastery={this.updateMastery}
-                />}
-                <div
-                    style={{
-                        borderRadius: '2.5em',
-                        border: 'solid 1px black',
-                        zIndex: 1000,
-                        maxHeight: 75,
-                        marginTop: 100,
-                        padding: 15,
-                    }}
-                >
-                    <TextField
-                        id='filter-input'
-                        label='Filter lessons...'
-                        value={this.state.filterInput}
-                        onChange={this.filterLessons}
+                <Grid item xs={3}>
+                    <Button
+                        variant='outlined'
+                        style={{borderRadius: '2.5em'}}
+                        onClick={() =>
+                            this.setState({lessons: []}, () => {
+                                const {allLessons, otherView} = this.state;
+                                const lessons = allLessons.filter(lesson => {
+                                    if (otherView === 'Completed') {
+                                        return lesson.mastery > 0.85;
+                                    } else {
+                                        return lesson.mastery <= 0.85;
+                                    }
+                                });
+                                console.log(otherView)
+                                console.log(lessons);
+                                this.setState({
+                                    lessons,
+                                    otherView: otherView === 'To Do' ? 'Completed' : 'To Do',
+                                });
+                            })
+                        }
+                    >
+                        View {this.state.otherView}
+                    </Button>
+                </Grid>
+                {this.state.lessons.length !== 0 && (
+                    <AVOLessonSlider
+                        theme={this.props.theme}
+                        changeToNewMastery={() => this.changeToNewMastery()}
+                        slides={this.state.lessons}
+                        updateMastery={this.updateMastery}
                     />
-                </div>
+                )}
+                <Grid container xs={12} style={{marginTop: 150}}>
+                    <Grid item xs={3}>
+                        <div
+                            style={{
+                                borderRadius: '2.5em',
+                                border: 'solid 1px black',
+                                padding: 15,
+                            }}
+                        >
+                            <TextField
+                                id='filter-input'
+                                style={{width: '100%'}}
+                                label='Filter lessons...'
+                                value={this.state.filterInput}
+                                onChange={this.filterLessons}
+                            />
+                        </div>
+                    </Grid>
+                    <Grid item xs={6}></Grid>
+                    <Grid item xs={3}>
+                        <div
+                            style={{
+                                marginLeft: 'auto',
+                                borderRadius: '2.5em',
+                                border: 'solid 1px black',
+                                padding: 15,
+                            }}
+                        >
+                            <Select
+                                style={{width: '100%'}}
+                                value={this.state.selectedClassName}
+                                input={<Input name='data' id='select-class' />}
+                                onChange={e =>
+                                    this.setState(
+                                        {selectedClassName: e.target.value as string},
+                                        () => this.changeClass(),
+                                    )
+                                }
+                            >
+                                {this.state.classNames.map((c: any, i: number) => (
+                                    <MenuItem key={i} value={c}>
+                                        {c}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </div>
+                    </Grid>
+                </Grid>
             </Grid>
         );
     }
 
     componentDidMount() {
-        this.getLessons();
+        this.getClasses();
     }
+
+    getClasses() {
+        Http.getClasses(
+            res => {
+                console.log(res);
+                const classes = res.classes;
+                if (classes.length > 0) {
+                    this.setState(
+                        {
+                            classNames: classes.map(c => c.name),
+                            classes,
+                            selectedClass: classes[0],
+                            selectedClassName: classes[0].name,
+                        },
+                        () => this.changeClass(),
+                    );
+                }
+            },
+            err => {
+                console.log(err);
+            },
+        );
+    }
+
+    changeClass = () => {
+        const {selectedClassName, classes} = this.state;
+        if (selectedClassName !== 'Select class...') {
+            const selectedClass = classes.find((c: Class) => c.name === selectedClassName);
+            if (selectedClass) {
+                this.setState({selectedClass}, () => this.getLessons());
+            }
+        }
+    };
+
     filterLessons = (e: any) => {
         const {allLessons} = this.state;
         const filterInput = e.target.value;
         if (filterInput === '') {
-            this.setState({filterInput, lessons: allLessons});
+            this.setState({
+                filterInput,
+                lessons: allLessons.filter(lesson => {
+                    if (this.state.otherView === 'Completed') {
+                        return lesson.mastery < 0.85;
+                    } else {
+                        return lesson.mastery >= 0.85;
+                    }
+                }),
+            });
         } else {
             const words = filterInput.split(' ');
-            const lessons = allLessons.filter(lesson => words.indexOf(lesson.Tag) !== -1);
+            const lessons = allLessons
+                .filter(lesson => words.indexOf(lesson.Tag) !== -1)
+                .filter(lesson => {
+                    if (this.state.otherView === 'Completed') {
+                        return lesson.mastery < 0.85;
+                    } else {
+                        return lesson.mastery >= 0.85;
+                    }
+                });
             this.setState({filterInput, lessons});
         }
     };
     getLessons = () => {
         Http.getLearnLessons(
             res => {
+                const {otherView} = this.state;
+                console.log(otherView);
                 console.log(res);
                 this.setState({
-                    lessons: res.lessons.map(x => ({...x, newMastery: x.mastery})),
+                    lessons: res.lessons
+                        .map(x => ({...x, newMastery: x.mastery}))
+                        .filter(lesson => {
+                            if (otherView === 'Completed') {
+                                return lesson.mastery < 0.85;
+                            } else {
+                                return lesson.mastery >= 0.85;
+                            }
+                        }),
                     allLessons: res.lessons.map(x => ({...x, newMastery: x.mastery})),
                 });
             },
