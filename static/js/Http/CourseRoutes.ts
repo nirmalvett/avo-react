d738f0c5d713a8ef300d92446e242f196855350e
Lionel Foxcroft <lionelf329@gmail.com>
2019-10-10 15:53:25 -0400

    avocadocore-dev-wiki#135: Add /getCourses route

--- static/js/Http/CourseRoutes.ts
+++ static/js/Http/CourseRoutes.ts
@@ -3,3 +3,14 @@ import {_request, cb} from './baseRequest';
 export function createCourse(name: string, success: cb<{}>, failure: cb) {
     _request('POST', '/createCourse', success, failure, {name});
 }
+
+interface GetCourses {
+    courses: {
+        courseID: number;
+        name: string;
+    }[];
+}
+
+export function getCourses(success: cb<GetCourses>, failure: cb) {
+    _request('POST', '/getCourses', success, failure);
+}
