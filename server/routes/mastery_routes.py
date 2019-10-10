a60cbc58bed6e49bef780a4c3b93960c383c2f61
Lionel Foxcroft <lionelf329@gmail.com>
2019-10-10 16:53:47 -0400

    avocadocore-dev-wiki#130: Add method stub for wrong answer survey

--- server/routes/mastery_routes.py
+++ server/routes/mastery_routes.py
@@ -11,14 +11,18 @@ MasteryRoutes = Blueprint('MasteryRoutes', __name__)
 @login_required
 @validate(conceptID=int, mastery=int, aptitude=int)
 def post_question_survey(concept_id: int, mastery: int, aptitude: int):
-    """
-    Edit an already existing message
-    :return: Confirmation that the message has been changed
-    """
     mastery_obj = Mastery.query.filter((Mastery.CONCEPT == concept_id) & (Mastery.USER == current_user.USER)).first()
     if mastery_obj is None:
         return jsonify(error='Mastery record not found')
     mastery_obj.mastery_survey = mastery
     mastery_obj.aptitude_survey = aptitude
     db.session.commit()
+    return jsonify({})
+
+
+@MasteryRoutes.route("/wrongAnswerSurvey", methods=['POST'])
+@login_required
+@validate(questionID=int, concepts=list)
+def post_question_survey(question_id: int, concepts: list):
+    # todo
     return jsonify({})
--- static/js/Http/MasteryRoutes.ts
+++ static/js/Http/MasteryRoutes.ts
@@ -3,3 +3,7 @@ import {_request, cb} from "./baseRequest";
 export function postQuestionSurvey(conceptID: number, mastery: number, aptitude: number, success: cb<{}>, failure: cb) {
     _request('POST', '/postQuestionSurvey', success, failure, {conceptID, mastery, aptitude});
 }
+
+export function wrongAnswerSurvey(questionID: number, concepts: number[], success: cb<{}>, failure: cb) {
+    _request('POST', '/wrongAnswerSurvey', success, failure, {questionID, concepts});
+}
