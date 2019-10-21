7a73f4f77a00c0f9d4c3c39ab1b978bd1b8add4e
Lionel Foxcroft <lionelf329@gmail.com>
2019-10-21 00:41:28 -0400

    avocadocore-dev-wiki#192: Fix /submitQuestion

--- server/routes/mastery_routes.py
+++ server/routes/mastery_routes.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List
 
 from flask import Blueprint, jsonify
@@ -5,7 +6,7 @@ from flask_login import current_user
 
 from server.MathCode.question import AvoQuestion
 from server.decorators import login_required, validate
-from server.models import Mastery, db, Question
+from server.models import Mastery, db, Question, ConceptQuestion, MasteryHistory
 
 MasteryRoutes = Blueprint('MasteryRoutes', __name__)
 
@@ -40,5 +41,40 @@ def submit_question(question_id: int, seed: int, answers: List[str]):
         q = AvoQuestion(question.string, seed, answers)
     except Exception as e:
         return jsonify(error="Question failed to be created", message=str(e))
-    # todo: do something to update mastery
+    concept_questions: List[ConceptQuestion] = ConceptQuestion.query.filter(
+        ConceptQuestion.QUESTION == question_id
+    ).all()
+    mastery: List[Mastery] = Mastery.query.filter(
+        (Mastery.USER == current_user.USER) &
+        (Mastery.CONCEPT == ConceptQuestion.CONCEPT) &
+        (ConceptQuestion.QUESTION == question_id)
+    ).all()
+
+    weight_dict = {}
+    for c in concept_questions:
+        weight_dict[c.CONCEPT] = c.weight
+
+    missing_concept_ids = set(weight_dict.keys())
+    for m in mastery:
+        missing_concept_ids.remove(m.CONCEPT)
+
+    for c in missing_concept_ids:
+        m = Mastery(c, current_user.USER, 0, 0, 0)
+        db.session.add(m)
+        mastery.append(m)
+    db.session.commit()
+
+    grade = 2 * q.score / sum(q.totals) - 1  # their grade ranges from -1 to 1
+
+    for m in mastery:
+        # <magic>
+        aptitude_factor = (m.aptitude_survey + 3) / 6 if m.aptitude_survey != 0 else 1
+        weight_factor = weight_dict[m.CONCEPT] / 4
+        baseline_change = 0.4
+        new_mastery = max(0, min(1, m.mastery_level + grade * weight_factor * aptitude_factor * baseline_change))
+        # </magic>
+        Mastery.mastery_level = new_mastery
+        db.session.add(MasteryHistory(m.MASTERY, new_mastery, datetime.now()))
+    db.session.commit()
+
     return jsonify(explanation=q.explanation, points=q.scores, totals=q.totals)
