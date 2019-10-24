ed5fbfa63ca97d7e04a945b9a689804fe8e03959
Lionel Foxcroft <lionelf329@gmail.com>
2019-10-24 17:39:15 -0400

    Fix Question Screen

--- static/js/Learn/LearnModal/QuestionScreen.tsx
+++ static/js/Learn/LearnModal/QuestionScreen.tsx
@@ -1,8 +1,8 @@
 import {AvoLessonData} from '../Learn';
-import {Button, Grid, Grow} from '@material-ui/core';
+import {Button} from '@material-ui/core';
 import {getMathJax} from '../../HelperFunctions/Utilities';
 import {AnswerInput} from '../../AnswerInput';
-import React from 'react';
+import React, {Fragment} from 'react';
 
 interface QuestionScreenProps {
     question: AvoLessonData;
@@ -15,51 +15,38 @@ interface QuestionScreenProps {
 export function QuestionScreen(props: QuestionScreenProps) {
     const question = props.question;
     return (
-        <Grow in={true} timeout={{enter: 1000}}>
-            <Grid container spacing={8}>
-                <Grid item xs={1} />
-                <Grid item xs={10} style={{position: 'relative'}}>
-                    <div
-                        style={{
-                            position: 'absolute',
-                            transition: 'transform 1s ease-in, opacity 500ms ease-in',
-                            willChange: 'transform',
-                            width: '100%',
-                            marginTop: '5em',
-                        }}
-                    >
-                        <div style={{textAlign: 'center'}}>
-                            {getMathJax(question.prompt)}
-                            {question.prompts.map((p, idx) => (
-                                <>
-                                    <br />
-                                    <br />
-                                    <div style={{textAlign: 'center'}}>
-                                        <AnswerInput
-                                            type={question.types[idx]}
-                                            value={props.answers[idx]}
-                                            prompt={p}
-                                            onChange={props.changeAnswer(idx)}
-                                            save={props.changeAnswer(idx)}
-                                        />
-                                    </div>
-                                </>
-                            ))}
-                        </div>
-                        <div style={{position: 'absolute', right: '4px', bottom: '4px'}}>
-                            <Button onClick={props.next} variant='outlined' color='primary'>
-                                Submit Answer
-                            </Button>
-                        </div>
-                    </div>
-                </Grid>
-                <Grid item xs={1} />
-                <div style={{position: 'absolute', left: '4px', top: '4px'}}>
-                    <Button onClick={props.back} variant='outlined' color='primary'>
-                        Go Back To Lesson
-                    </Button>
-                </div>
-            </Grid>
-        </Grow>
+        <Fragment>
+            <div
+                style={{
+                    flex: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    textAlign: 'center',
+                    overflowY: 'auto',
+                    padding: '16px',
+                }}
+            >
+                {getMathJax(question.prompt)}
+                {question.prompts.map((p, idx) => (
+                    <AnswerInput
+                        type={question.types[idx]}
+                        value={props.answers[idx]}
+                        prompt={p}
+                        onChange={props.changeAnswer(idx)}
+                        save={props.changeAnswer(idx)}
+                    />
+                ))}
+            </div>
+            <div style={{position: 'absolute', left: '16px', top: '4px'}}>
+                <Button onClick={props.back} variant='outlined' color='primary'>
+                    Go Back To Lesson
+                </Button>
+            </div>
+            <div style={{position: 'absolute', right: '16px', bottom: '4px'}}>
+                <Button onClick={props.next} variant='outlined' color='primary'>
+                    Submit Answer
+                </Button>
+            </div>
+        </Fragment>
     );
 }
