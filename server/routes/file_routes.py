0110a3aced7045a2cb9f3029e7263536f2362f44
Owen <owendot2011@gmail.com>
2019-12-04 20:54:11 -0500

    uploading to aws working

--- server/routes/file_routes.py
+++ server/routes/file_routes.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, send_file, send_from_directory, request, session
+from flask import Blueprint, send_file, send_from_directory, request, session, jsonify
 import os
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = os.getcwd() + '/tmp/'
+import boto3
+
+S3_BUCKET = 'avo-frontend'
+
+S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
 FileRoutes = Blueprint('FileRoutes', __name__)
 
@@ -20,12 +24,26 @@ def serve_dist_file(filename):
 
 @FileRoutes.route('/upload', methods=['POST'])
 def file_upload():
-    target = UPLOAD_FOLDER
-    if not os.path.isdir(target):
-        os.mkdir(target)
-    file = request.files['file']
+    file = request.files.get('file')
+    if file is None:
+        return 'Error: please send a file'
     filename = secure_filename(file.filename)
-    destination = "/".join([target, filename])
-    file.save(destination)
-    session['uploadFilePath'] = destination
-    return filename
+
+    try:
+        client = boto3.client('s3', region_name='us-east-2')
+
+        client.upload_fileobj(
+            file,
+            S3_BUCKET,
+            filename,
+            ExtraArgs={
+                "ACL": 'public-read',
+                "ContentType": file.content_type
+            }
+        )
+
+    except Exception as e:
+        print(e)
+        return 'Error: could not upload'
+
+    return "{}{}".format(S3_LOCATION, file.filename)
