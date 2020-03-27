6738cb35a7b0bd40af99bf86228f9ba529e1a24f
Owen <owendot2011@gmail.com>
2020-03-26 21:10:29 -0400

    var to const

--- static/js/QuestionBuilder/WordInputQuestionBuilder.tsx
+++ static/js/QuestionBuilder/WordInputQuestionBuilder.tsx
@@ -747,7 +747,7 @@ export default class WordInputQuestionBuilder extends Component<WordInputQuestio
                 e.preventDefault();
 
                 // get text representation of clipboard
-                var text = e.clipboardData.getData('text/plain');
+                const text = e.clipboardData.getData('text/plain');
                 // insert text manually
                 document.execCommand("insertHTML", false, text);
             });
