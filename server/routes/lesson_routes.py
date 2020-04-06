ee76b68d6ff8314def189e012ef55a84208edfdc
Michael Ens <ens.michael99@gmail.com>
2020-04-06 00:10:16 -0400

    added all lesson routes

--- server/routes/lesson_routes.py
+++ server/routes/lesson_routes.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask import Blueprint, jsonify
 
 from server import db
-from server.auth import able_edit_course
+from server.auth import able_edit_course, able_view_course
 from server.decorators import login_required, teacher_only, validate
 from server.models import Concept, Lesson
 
@@ -26,20 +26,43 @@ def add_lesson(course_ID: int, concept_ID: int, content: str, has_assignment: bo
 
 @LessonRoutes.route('/editLesson', methods=['POST'])
 @teacher_only
-@validate()
-def edit_lesson():
-    return ''  # todo
+@validate(lessonID=int, conceptID=int, content=str, hasAssignment=bool, dueDate=datetime)
+def edit_lesson(lesson_ID: int, concept_ID: int, content: str, has_assignment: bool, due_date: datetime):
+    lesson = Lesson.query.get(lesson_ID)
+    if lesson is None:
+        return jsonify(error='Lesson not found')
+    if not able_edit_course(lesson.COURSE):
+        return jsonify(error="Course not found or not able to edit course")
+    concept = Concept.query.get(concept_ID)
+    if concept is None:
+        return jsonify(error="Concept not found")
+    lesson.CONCEPT = concept_ID
+    lesson.content = content
+    lesson.has_assignment = has_assignment
+    lesson.due_date = due_date
+    db.session.commit()
+    return jsonify(code=1)
 
 
 @LessonRoutes.route('/deleteLesson', methods=['POST'])
 @teacher_only
-@validate()
-def delete_lesson():
-    return ''  # todo
+@validate(lessonID=int)
+def delete_lesson(lesson_ID: int):
+    lesson = Lesson.query.get(lesson_ID)
+    if lesson is None:
+        return jsonify(error="No lesson found")
+    if not able_edit_course(lesson.COURSE):
+        return jsonify(error="User not able to edit course")
+    db.session.delete(lesson)
+    db.session.commit()
+    return jsonify(code=1)
 
 
 @LessonRoutes.route('/getLessons', methods=['POST'])
 @login_required
-@validate()
-def get_lessons():
-    return ''
+@validate(courseID=int)
+def get_lessons(course_id: int):
+    if not able_view_course(course_id):
+        return jsonify(error="User not able to view course or course does not exist")
+    lessons = Lesson.query.filter(Lesson.COURSE == course_id)
+    return jsonify(lessons=lessons)
