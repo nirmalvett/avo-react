a2cad83e14853160e4f1a683a783d8f3f181a109
Owen <owendot2011@gmail.com>
2020-04-24 19:37:14 -0400

    refactoring, added file table to unfiy how we're dealing with files

--- server/routes/file_upload_routes.py
+++ server/routes/file_upload_routes.py
@@ -4,7 +4,7 @@ from werkzeug.utils import secure_filename
 from server import db
 from server.auth import able_view_course
 from server.decorators import teacher_only, login_required
-from server.models import Assignment, Image, Lesson
+from server.models import Assignment, File, Lesson, FileType
 from config import BUCKET_NAME
 from google.cloud import storage
 import tempfile
@@ -16,15 +16,17 @@ PREFIX = f'gs://{BUCKET_NAME}/'
 @FileUploadRoutes.route('/assignment/<filename>')
 @login_required
 def assignment(filename):
-    a = Assignment.query\
-        .join(Lesson, Lesson.LESSON == Assignment.LESSON)\
-        .filter(Assignment.name == filename).first()
+    a = Assignment.query \
+        .join(File, File.FILE == Assignment.FILE) \
+        .join(Lesson, Lesson.LESSON == Assignment.LESSON) \
+        .filter(File.file_name == filename)\
+        .first()
     # check if the user is the student who submitted the assignment or the teacher who made the assignment
     if current_user.USER != a.USER or current_user.USER != a.LESSON_RELATION.USER:
         return jsonify(error='User does not own assignment')
     storage_client = storage.Client()
-    bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(a.name)
+    bucket = storage_client.bucket(a.FILE_RELATION.bucket)
+    blob = bucket.blob(a.FILE_RELATION.file_name)
     if blob.exists(storage_client):
         fp = tempfile.TemporaryFile()
         blob.download_to_file(fp)
@@ -36,10 +38,15 @@ def assignment(filename):
 @FileUploadRoutes.route('/image/<filename>')
 @login_required
 def image(filename):
-    i: Image = Image.query.filter(Image.name == filename).first()
+    image = File.query \
+        .join(FileType, FileType.FILE_TYPE == File.FILE_TYPE) \
+        .filter((FileType.name == 'image') & (File.file_name == filename)) \
+        .first()
+    if image is None:
+        return jsonify(error=f'Could not find image {filename}')
     storage_client = storage.Client()
-    bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(i.name)  # TODO: Figure out how we want to deal with security for images
+    bucket = storage_client.bucket(image.bucket)
+    blob = bucket.blob(image.file_name)  # TODO: Figure out how we want to deal with security for images
     if blob.exists(storage_client):
         fp = tempfile.TemporaryFile()
         blob.download_to_file(fp)
@@ -51,10 +58,11 @@ def image(filename):
 @FileUploadRoutes.route('/getImages')
 @teacher_only
 def get_images():
-    images = Image.query.filter(
-        (Image.USER == current_user.USER)
-    ).all()
-    return jsonify({'images': {i.IMAGE: i.name for i in images}})
+    images = File.query \
+        .join(FileType, FileType.FILE_TYPE == File.FILE_TYPE) \
+        .filter((FileType.name == 'image') & (File.USER == current_user.USER)) \
+        .all()
+    return jsonify({'images': {i.IMAGE: i.file_name for i in images}})
 
 
 @FileUploadRoutes.route('/upload/image', methods=['POST'])
@@ -63,7 +71,7 @@ def image_upload():
     file = request.files.get('file')
     if file is None:
         return 'Error: please send a file'
-    filename = f'{current_user.USER}__{secure_filename(file.filename)}'
+    filename = f'images/{current_user.USER}/{secure_filename(file.filename)}'
     try:
         storage_client = storage.Client()
         bucket = storage_client.bucket(BUCKET_NAME)
@@ -72,18 +80,21 @@ def image_upload():
     except Exception as e:
         return jsonify(error=f'Error could not save file: {e}')
 
-    url = f'{PREFIX}{filename}'
-    i: Image = Image.query.filter(
-        (Image.name == secure_filename(file.filename)) &
-        (Image.USER == current_user.USER)
+    file = File.query.filter(
+        (File.file_name == filename) &
+        (File.USER == current_user.USER)
     ).first()
-    if i is None:
-        img = Image(name=secure_filename(filename), url=url, user_id=current_user.USER)
-        db.session.add(img)
-    else:
-        i.url = url
-    db.session.commit()
-    return url
+    if file is None:
+        file_type = FileType.query.filter(FileType.name == 'image').first()
+        file = File(
+            file_name=filename,
+            user_id=current_user.USER,
+            file_type=file_type.FILE_TYPE,
+            bucket=BUCKET_NAME,
+        )
+        db.session.add(file)
+        db.session.commit()
+    return jsonify(file_name=filename)
 
 
 @FileUploadRoutes.route('/upload/assignment/<lesson_id>', methods=['POST'])
@@ -97,7 +108,7 @@ def assignment_upload(lesson_id: int):
     file = request.files.get('file')
     if file is None:
         return 'Error: please send a file'
-    filename = f'{lesson.LESSON}__{current_user.USER}__{secure_filename(file.filename)}'
+    filename = f'assignments/{lesson.LESSON}/{current_user.USER}/{secure_filename(file.filename)}'
 
     try:
         storage_client = storage.Client()
@@ -106,13 +117,21 @@ def assignment_upload(lesson_id: int):
         blob.upload_from_file(file)
     except Exception as e:
         return jsonify(error=f'Error could not save file: {e}')
-    url = f'{PREFIX}{filename}'
-    assignment = Assignment.query.filter((Assignment.name == secure_filename(file.filename)) &
-                                         (Assignment.USER == current_user.USER)).first()
+    assignment = Assignment.query \
+        .join(File, File.FILE == Assignment.FILE) \
+        .filter((File.file_name == filename) & (Assignment.USER == current_user.USER))\
+        .first()
     if assignment is None:
-        assignment = Assignment(lesson=lesson_id, name=secure_filename(filename), user=current_user.USER, url=url)
+        file_type = FileType.query.filter(FileType.name == 'assignment').first()
+        file = File(
+            file_name=filename,
+            user_id=current_user.USER,
+            file_type=file_type.FILE_TYPE,
+            bucket=BUCKET_NAME,
+        )
+        db.session.add(file)
+        db.session.flush()
+        assignment = Assignment(lesson=lesson_id, file=file.FILE, user=current_user.USER)
         db.session.add(assignment)
-    else:
-        assignment.url = url
-    db.session.commit()
-    return url
+        db.session.commit()
+    return jsonify(file_name=filename)
