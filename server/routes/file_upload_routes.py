761a88fd6d78aaec4720bcc3403444f2cb3fbf1d
Owen <owendot2011@gmail.com>
2020-04-24 03:01:02 -0400

    changed file uploading/fetching to use google cloud

--- server/routes/file_upload_routes.py
+++ server/routes/file_upload_routes.py
@@ -1,24 +1,54 @@
-from flask import Blueprint, abort, redirect, jsonify, request
+from flask import Blueprint, jsonify, request, send_file
 from flask_login import current_user
 from werkzeug.utils import secure_filename
-import boto3
 from server import db
 from server.auth import able_view_course
-from server.decorators import teacher_only, login_required, validate
+from server.decorators import teacher_only, login_required
 from server.models import Assignment, Image, Lesson
-from config import S3_BUCKET, S3_LOCATION
+from config import BUCKET_NAME
+from google.cloud import storage
+import tempfile
 
 FileUploadRoutes = Blueprint('ImageRoutes', __name__)
+PREFIX = f'gs://{BUCKET_NAME}/'
+
+
+@FileUploadRoutes.route('/assignment/<filename>')
+@login_required
+def assignment(filename):
+    a = Assignment.query\
+        .join(Lesson, Lesson.LESSON == Assignment.LESSON)\
+        .filter(Assignment.name == filename).first()
+    # check if the user is the student who submitted the assignment or the teacher who made the assignment
+    if current_user.USER != a.USER or current_user.USER != a.LESSON_RELATION.USER:
+        return jsonify(error='User does not own assignment')
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blob_name = a.url.replace(PREFIX, '')
+    blob = bucket.blob(blob_name)
+    if blob.exists(storage_client):
+        fp = tempfile.TemporaryFile()
+        blob.download_to_file(fp)
+        fp.seek(0)
+        return send_file(fp, mimetype='application/octet-stream')
+    return jsonify(error='assignment not found')
 
 
 @FileUploadRoutes.route('/image/<filename>')
 @login_required
 def image(filename):
     i: Image = Image.query.filter(Image.name == filename).first()
-    if i is None:
-        return abort(404)
-    else:
-        return redirect(i.url, code=302)
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blob_name = i.url.replace(PREFIX, '')
+    blob = bucket.blob(blob_name)  # TODO: Figure out how we want to deal with security for images
+    if blob.exists(storage_client):
+        fp = tempfile.TemporaryFile()
+        blob.download_to_file(fp)
+        fp.seek(0)
+        return send_file(fp, mimetype='application/octet-stream')
+    return jsonify({'error': 'image not found'})
 
 
 @FileUploadRoutes.route('/getImages')
@@ -37,25 +67,15 @@ def image_upload():
     if file is None:
         return 'Error: please send a file'
     filename = f'{current_user.USER}__{secure_filename(file.filename)}'
-
     try:
-        # need to run `aws configure` to link account so this works
-        client = boto3.client('s3', region_name='ca-central-1')
-
-        client.upload_fileobj(
-            file,
-            S3_BUCKET,
-            filename,
-            ExtraArgs={
-                "ACL": 'public-read',
-                "ContentType": file.content_type
-            }
-        )
-
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(BUCKET_NAME)
+        blob = bucket.blob(filename)
+        blob.upload_from_file(file)
     except Exception as e:
-        print(e)
-        return 'Error: could not upload'
-    url = "{}{}".format(S3_LOCATION, filename)
+        return jsonify(error=f'Error could not save file: {e}')
+
+    url = f'{PREFIX}{filename}'
     i: Image = Image.query.filter(
         (Image.name == secure_filename(file.filename)) &
         (Image.USER == current_user.USER)
@@ -83,23 +103,13 @@ def assignment_upload(lesson_id: int):
     filename = f'{lesson.LESSON}__{current_user.USER}__{secure_filename(file.filename)}'
 
     try:
-        # need to run `aws configure` to link account so this works
-        client = boto3.client('s3', region_name='ca-central-1')
-
-        client.upload_fileobj(
-            file,
-            S3_BUCKET,
-            filename,
-            ExtraArgs={
-                "ACL": 'public-read',
-                "ContentType": file.content_type
-            }
-        )
-
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(BUCKET_NAME)
+        blob = bucket.blob(filename)
+        blob.upload_from_file(file)
     except Exception as e:
-        print(e)
-        return 'Error: could not upload'
-    url = "{}{}".format(S3_LOCATION, filename)
+        return jsonify(error=f'Error could not save file: {e}')
+    url = f'{PREFIX}{filename}'
     assignment = Assignment.query.filter((Assignment.name == secure_filename(file.filename)) &
                                          (Assignment.USER == current_user.USER)).first()
     if assignment is None:
@@ -109,4 +119,3 @@ def assignment_upload(lesson_id: int):
         assignment.url = url
     db.session.commit()
     return url
-
